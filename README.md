6fa4268d2f42072ee53ef5511401e884c1c51c12
jk <47693+sectore@users.noreply.github.com>
2024-02-23 16:19:51 +0100

    add <bitcoin-logo /> component and stuff (#1)

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# bitcoin-logo-webcomponent
-&lt;bitcoin-logo /> web component
+# `<bitcoin-logo />`
+
+(Official) `Bitcoin` logo provided as [Web Component](https://developer.mozilla.org/en-US/docs/Web/API/Web_components). It based on the original graphic promoted by `bitboy` in 2010 as described by [`Bitcoin Design`](https://bitcoin.design) [here](https://bitcoin.design/guide/getting-started/visual-language/#update-by-bitboy).
+
+The SVG itself is friendly borrowed from `Bitcoin Design` ([source](https://bitcoin.design/assets/images/guide/getting-started/visual-language/bitcoin-symbol.svg)).
+
+## Features
+
+- Customize size and colors as you like
+- SVG based
+- No dependencies
+- Small bundle size: `3.14 kB │ gzip: 1.53 kB`
+- Supported by modern browsers
+- Use it with your favourite web framework
+
+## Usage
+
+### Installation 
+
+Use your favourite Node package manager to install `bitcoin-logo` web component.
+
+`npm`
+
+```bash
+npm install @bitcoinbeachtravemuende/bitcoin-logo
+```
+
+`bun`
+
+```bash
+bun add @bitcoinbeachtravemuende/bitcoin-logo
+```
+
+`pnpm`
+
+```bash
+pnpm add @bitcoinbeachtravemuende/bitcoin-logo
+```
+
+`yarn`
+
+```bash
+yarn add @bitcoinbeachtravemuende/bitcoin-logo
+```
+
+### Use bundle via `unpkg`
+
+TBD
+
+### Properties
+
+- `size: string`: `px` or `em`, eg. `size="300px"` or `size="8em"`
+- `background-color: string`: Background color. Value can be either a valid color `name` or a `hex` color, eg. `bgColor="white"` or `bgColor="#FFF"`
+- `color: string`: Color of the capital `₿`. Value can be either a valid color `name` or a `hex` color, eg. `color="black"` or `color="#000"`
+
+### Examples
+
+**_Default_**
+
+```html
+<bitcoin-logo />
+```
+
+**_Custom colors_**
+
+```html
+<bitcoin-logo color="white" background-color="black"></bitcoin-logo>
+```
+
+
+**_All custom values_**
+
+```html
+<bitcoin-logo size="300px" background-color="black" color="white"></bitcoin-logo>
+```
+
+## Build
+
+### Requirements
+
+#### Nix
+
+Install [Nix](https://zero-to-flakes.com/install)
+
+#### Other
+
+Install [`pnpm`](https://pnpm.io/) or any other Node package manager ([`bun`](https://bun.sh/), [`npm`](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm), [`yarn`](https://yarnpkg.com/) or whatever)
+
+### Local build
+
+- Install dependencies (command might be different for other package manager)
+
+```bash
+pnpm install
+```
+
+- Local build
+
+```bash
+pnpm dev
+```
+Open http://localhost:5173
+
+- Release build
+
+```bash
+pnpm build
+```
+Build sources land into `dist` folder.
+
+## License
+
+[MIT](./LICENSE)
---
+++ src/bitcoin-logo.ts
@@ -0,0 +1,91 @@
+export type Properties = {
+  size?: string;
+  color?: string;
+  "background-color"?: string;
+};
+
+const attributes: (keyof Properties)[] = ["size", "color", "background-color"];
+
+const DEFAULT_SIZE = "140px";
+const DEFAULT_COLOR = "white";
+const DEFAULT_BG_COLOR = "#F7931A";
+
+/**
+ * @attribute {string} size Size to determine width and height of the logo
+ * @attribute {string} bgColor Background color of the logo
+ * @attribute {string} color Color of the capital `₿`
+ */
+class BitcoinLogo extends HTMLElement {
+  shadowRoot: ShadowRoot;
+
+  constructor() {
+    super();
+    this.shadowRoot = this.attachShadow({ mode: "open" });
+  }
+
+  static get observedAttributes(): string[] {
+    return [...attributes];
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    // dirty check
+    if (oldValue === newValue) return;
+
+    const logo = this.shadowRoot.getElementById("logo");
+    // path of `₿`
+    const b = this.shadowRoot.getElementById("₿");
+    // path of background
+    const bg = this.shadowRoot.getElementById("₿₿");
+
+    switch (name) {
+      case "size": {
+        logo?.style.setProperty("width", newValue || DEFAULT_SIZE);
+        logo?.style.setProperty("height", newValue || DEFAULT_SIZE);
+        break;
+      }
+      case "background-color": {
+        bg?.setAttribute("fill", newValue || DEFAULT_BG_COLOR);
+        break;
+      }
+      case "color": {
+        b?.setAttribute("fill", newValue || DEFAULT_COLOR);
+        break;
+      }
+    }
+  }
+
+  connectedCallback() {
+    const logo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    logo.setAttribute("id", "logo");
+    logo.setAttribute("width", "100%");
+    logo.setAttribute("height", "100%");
+    logo.setAttribute("viewBox", "0 0 144 145");
+    logo.innerHTML = `<path id="₿₿" d="M141.846 89.665C132.225 128.236 93.1561 151.711 54.5761 142.093C16.0121 132.476 -7.46593 93.408 2.15707 54.839C11.7711 16.264 50.8431 -7.21401 89.4101 2.40299C127.985 12.02 151.462 51.091 141.846 89.665Z" fill=${
+      this.getAttribute("background-color") || DEFAULT_BG_COLOR
+    } />
+    <path id="₿" d="M103.736 61.991C105.169 52.409 97.873 47.26 87.898 43.824L91.133 30.846L83.234 28.877L80.083 41.513C78.004 40.995 75.873 40.507 73.752 40.024L76.923 27.304L69.029 25.335L65.79 38.311C64.071 37.919 62.384 37.533 60.745 37.125L60.753 37.085L49.859 34.365L47.757 42.802C47.757 42.802 53.618 44.145 53.494 44.229C56.694 45.028 57.271 47.145 57.175 48.823L53.49 63.608C53.71 63.665 53.995 63.744 54.311 63.872C54.047 63.806 53.766 63.735 53.477 63.665L48.311 84.375C47.919 85.347 46.926 86.806 44.69 86.252C44.769 86.366 38.945 84.819 38.945 84.819L35.025 93.86L45.305 96.423C47.216 96.902 49.093 97.402 50.937 97.876L47.67 111.003L55.561 112.972L58.798 99.985C60.956 100.569 63.048 101.11 65.096 101.619L61.87 114.545L69.77 116.514L73.04 103.412C86.513 105.96 96.644 104.932 100.906 92.749C104.343 82.939 100.736 77.28 93.648 73.589C98.809 72.399 102.699 69.003 103.736 61.991ZM85.687 87.302C83.243 97.113 66.727 91.809 61.369 90.478L65.707 73.089C71.062 74.424 88.237 77.07 85.687 87.302ZM88.127 61.85C85.901 70.774 72.152 66.239 67.692 65.127L71.624 49.355C76.083 50.467 90.448 52.542 88.127 61.85Z" fill=${
+      this.getAttribute("color") || DEFAULT_COLOR
+    } />`;
+
+    const style = document.createElement("style");
+
+    style.textContent = `
+      #logo {
+        height: ${this.getAttribute("size") || DEFAULT_SIZE};
+        width: ${this.getAttribute("size") || DEFAULT_SIZE};
+      }`;
+
+    this.shadowRoot.appendChild(logo);
+    this.shadowRoot.appendChild(style);
+  }
+}
+
+customElements.define("bitcoin-logo", BitcoinLogo);
+
+export default BitcoinLogo;
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "bitcoin-logo": typeof BitcoinLogo;
+  }
+}
